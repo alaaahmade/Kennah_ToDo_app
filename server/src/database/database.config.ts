188363ea34b58ce1d0833e8ac172aa6022052a2a
Alaa Ahmad <100975982+alaaahmade@users.noreply.github.com>
2025-04-19 11:46:46 +0300

    Delete server directory

--- server/src/database/database.config.ts
+++
@@ -1,9 +0,0 @@
-import { MongooseModule } from '@nestjs/mongoose';
-import * as dotenv from 'dotenv';
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-dotenv.config();
-
-export const DatabaseConfig = MongooseModule.forRoot(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/nestjs-api',
-);
